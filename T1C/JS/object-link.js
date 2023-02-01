cc82ed69f01909d96ee153a910a50fcb9753fd6b
Alejandro Vivanco <manuelinviv@gmail.com>
2023-01-31 23:50:26 -0500

    Object links update

--- T1C/JS/object-link.js
+++ T1C/JS/object-link.js
@@ -1,13 +1,33 @@
 var Person = {
-  printName: function (){
-    console.log(`My name is ${this.name} and I am ${this.age} years old.`)
-  }
-}
+  printName: function () {
+    console.log(`My name is ${this.name} and I am ${this.age} years old.`);
+  },
+};
 
-var alejo = Object.create(Person)
+var SoftwareDeveloper = Object.create(Person);
+SoftwareDeveloper.presentation = function () {
+  console.log(
+    `Hello World! I'm ${this.name} and I'm a ${this.area} developer.`
+  );
+};
+SoftwareDeveloper.printName = function () {
+  console.log("Polymorphism simulation")
+};
 
-alejo.name = "Alejandro"
-alejo.age = "24"
+var alejoDeveloper = Object.create(SoftwareDeveloper);
+alejoDeveloper.name = "Alejandro";
+alejoDeveloper.area = "Full stack";
 
-alejo.printName()
-//My name is Alejandro and I am 24 years old.
+console.log(alejoDeveloper.presentation());
+//Hello World! I'm Alejandro and I'm a Full stack developer.
+// We can simulate subclasses using prototypal delegation, Person is the main class and SoftwareDeveloper the subclass
+console.log(alejoDeveloper.printName())
+// Polymorphism simulation
+console.log(Object.getOwnPropertyNames(alejoDeveloper))
+// ['name', 'area']
+console.log(alejoDeveloper.hasOwnProperty('age'))
+// false because this is a property from Person
+console.log(alejoDeveloper.hasOwnProperty('area'))
+// true because this is a property from SoftwareDeveloper
+console.log(Object.getPrototypeOf(alejoDeveloper).hasOwnProperty("presentation"));
+// true because this comes from SoftwareDeveloper (parent)
